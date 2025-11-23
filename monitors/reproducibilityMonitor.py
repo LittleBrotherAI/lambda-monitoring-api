5e0f93fcd1914ea5af3a5f7b402874345df1d99c
Rafi Hakim <raha00004@stud.uni-saarland.de>
2025-11-23 15:41:05 +0100

    bug fixes

--- monitors/reproducibilityMonitor.py
+++ monitors/reproducibilityMonitor.py
@@ -1,7 +1,7 @@
-import os
 import requests
-from ..utils.ollama_api import call_little_brother
-from ..utils.utils import answer_similarity
+import asyncio
+from utils.ollama_api import call_little_brother
+from utils.utils import answer_similarity
 
 async def monitorReproducibility(message_id:str, url:str, prompt:str, cot:str, response:str)->float: 
 
@@ -19,9 +19,16 @@ async def monitorReproducibility(message_id:str, url:str, prompt:str, cot:str, r
     
     little_brother_response = call_little_brother(messages=messages, thinking="low") #default is gpt-oss:120b as model, adjust to your needs through argument (temp as well)
 
-
     # compute your similarities
     similarity = answer_similarity(response, little_brother_response)
     requests.post(url, json={"reproducibility_score": similarity, "message_id":message_id})
-
     return similarity
+
+if __name__ == "__main__":
+    prompt = "Ths is a smple txt with sme nonsensical wrds and 1234 numbers."
+    cot = "todo"
+    response = "stuff"
+    result = asyncio.run(
+        monitorReproducibility("whatever", "stuff", prompt, cot, response)
+    )
+    print(result)
